17c3d126dca5d7acbc151709b8d7078fa2a3ade0
Sumifit Business <sumifitbr@gmail.com>
2024-09-02 14:00:04 +0000

    nova script read file csv local

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # auctions
 Projeto de integração de dados imóveis em leilões
+
+
+## Diretório da aplicação
+
+``` /app ```
+
+## Efetuando download com WGET
+
+``` wget https://venda-imoveis.caixa.gov.br/listaweb/Lista_imoveis_TO.csv?27030242 ```
+
+
+## Sobre o projeto
+
+A ideia inicial é efetuar o donwload dos arquivos da caixa todos os dias pela manhã e com isso efetuar o insert na base de dados PGSQL
+
+Após isso, iremos com a *GOOGLE PLACES API*, efetuarmos o enriquecimentos dos dados, buscando lugaremos próximos bem como a média de valores dos imóveis ao redor do endereço de cada imóvel listado na base de dados.
+
+google-chrome --headless --no-sandbox --disable-gpu --dump-dom https://venda-imoveis.caixa.gov.br/listaweb/Lista_imoveis_TO.csv?27030242
---
+++ app/load_csv_to_pgsql.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from datetime import datetime
+import os
+
+# Diretório onde os arquivos CSV estão armazenados
+files_dir = '/app/files'
+
+# Configurações de conexão com o PostgreSQL
+db_user = "postgres"
+db_password = "Last@1981"
+db_host = "94.130.107.131"
+db_port = "5432"
+db_name = "auction_dev"
+
+# Criar a engine de conexão
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+
+# Nome da tabela final
+table_name = "public.tbl_imoveis"
+
+# Lista para armazenar DataFrames temporários
+dfs = []
+
+# Listar todos os arquivos CSV no diretório
+csv_files = [f for f in os.listdir(files_dir) if f.endswith('.csv')]
+
+for csv_file in csv_files:
+    try:
+        # Construir o caminho completo do arquivo
+        file_path = os.path.join(files_dir, csv_file)
+
+        # Extrair o estado do nome do arquivo
+        estado = csv_file.split('_')[-1].replace('.csv', '')
+
+        # Ler o CSV, pulando as duas primeiras linhas
+        df = pd.read_csv(file_path, sep=';', encoding='latin1', skiprows=2)
+
+        # Adicionar coluna com o estado
+        df['estado'] = estado
+
+        # Inserir a coluna etl_load_date
+        df['etl_load_date'] = datetime.now()
+
+        # Adicionar DataFrame à lista
+        dfs.append(df)
+
+        print(f"Dados processados e preparados para o arquivo {csv_file}.")
+    except Exception as e:
+        print(f"Erro ao processar o arquivo {csv_file}: {e}")
+
+# Verificar se há DataFrames antes de concatenar
+if dfs:
+    # Concatenar todos os DataFrames
+    df_consolidado = pd.concat(dfs, ignore_index=True)
+
+    # Inserir os dados na tabela final no PostgreSQL
+    df_consolidado.to_sql(table_name, engine, if_exists='replace', index=False)
+
+    print(f"Dados consolidados inseridos com sucesso na tabela '{table_name}'.")
+else:
+    print("Nenhum dado foi processado. Verifique os erros e tente novamente.")
